3ca2ec31039653748a2fd4365afb60baff03ac05
NikolayBlagoev <workerthreadllc@gmail.com>
2025-07-07 21:39:06 +0300

    test fix due to ubuntu

--- eval_best_answer.py
+++ eval_best_answer.py
@@ -38,10 +38,10 @@ def generate_prompt_bg(q_id, shots = 8):
 
         
         
-        txt += f"Q: {generate_ds["question"][el]}"
-        txt += f"\nA: Нека решим задачата стъпка по стъпка. {generate_ds["answer"][el]}. Тъй че отговорът е {generate_ds["num_answer"][el]}\n"
+        txt += f"Q:" + generate_ds["question"][el]
+        txt += f"\nA: Нека решим задачата стъпка по стъпка. " + generate_ds["answer"][el] + ". Тъй че отговорът е " + {generate_ds["num_answer"][el]} + "\n"
     generate_ds = run(1,[q_id,q_id+1])
-    txt += f"Q: {generate_ds["question"][0]}"
+    txt += f"Q: " + generate_ds["question"][0]
     txt += f"\nA: "
     return txt, generate_ds["num_answer"][0], generate_ds["answer"][0], generate_ds["question"][0]
 
